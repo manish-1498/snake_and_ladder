e7ac95e06148ce0d68d392bc7949fbc0715f6f63
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-21 22:16:46 +0530

    [manish]:added:winning position till100

--- snakeandladderproblem/snakeandladder.cs
+++ snakeandladderproblem/snakeandladder.cs
@@ -9,29 +9,35 @@ namespace snakeandladderproblem
         public void sandl()
         {
             int playerpos= 0;
-            Random random = new Random();
-            int roll = random.Next(1, 7);
             
-            int option = random.Next(1, 3);
-            switch (option)
+            while (playerpos < 100)
             {
-                case 1:
-                    Console.WriteLine("roll value is" + roll);
-                    Console.WriteLine("Ladder");
-                    playerpos += roll;
-                    Console.WriteLine("player position is :" + playerpos);
-                    break;
-                case 2:
-                    Console.WriteLine("roll value is" + roll);
-                    Console.WriteLine("Snake");
-                    playerpos -= roll;
-                    if (playerpos < 0)
-                        playerpos = 0;
-                    Console.WriteLine("player position is :" + playerpos);
-                    break;
-                default:
-                    Console.WriteLine("No play");
-                    break;
+                Random random = new Random();
+                int roll = random.Next(1, 7);
+                int option = random.Next(1, 3);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("roll value is" + roll);
+                        Console.WriteLine("Ladder");
+                        playerpos += roll;
+                        if (playerpos > 100)
+                            playerpos -= roll;
+                        Console.WriteLine("player position is :" + playerpos);
+                        break;
+                    case 2:
+                        Console.WriteLine("roll value is" + roll);
+                        Console.WriteLine("Snake");
+                        playerpos -= roll;
+                        if (playerpos < 0)
+                            playerpos = 0;
+                        Console.WriteLine("player position is :" + playerpos);
+                        break;
+                    default:
+                        Console.WriteLine("No play");
+                        break;
+                }
+                
             }
 
         }
