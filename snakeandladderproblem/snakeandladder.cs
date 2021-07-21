b1e95aa3061d36948c3c26e94ae1cfb2de05f9e3
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-21 20:18:59 +0530

    [manish]added:dieroll

--- snakeandladderproblem/snakeandladder.cs
+++ snakeandladderproblem/snakeandladder.cs
@@ -8,8 +8,12 @@ namespace snakeandladderproblem
     {
         public void sandl()
         {
-            int player = 0;
-            Console.WriteLine("player is:"+player);
+            int playerpos= 0;
+            Console.WriteLine("player position is"+playerpos);
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            playerpos += roll;
+            Console.WriteLine("player's new position is:"+playerpos);
         }
     }
 }
